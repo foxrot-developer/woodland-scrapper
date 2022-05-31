16f392e9424fc56d97f96b35193c3277ed4531ae
RajaSabiq <ther.sabiqarfan@gmail.com>
2022-05-31 13:56:20 +0500

    Firebase setup

--- index.js
+++ index.js
@@ -7,32 +7,34 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'GET')
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  next();
 });
 
 app.get('/api/woodland', async (req, res, next) => {
-    browserConfiguration().then(result => {
-        console.log("Code Executed successfully .....");
-    });
-    res.send('Execution started ...');
+  browserConfiguration().then((result) => {
+    console.log('Code Executed successfully .....');
+  });
+  res.send('Execution started ...');
 });
 
 app.use((req, res, next) => {
-    throw new HttpError('Could not find the route', 404);
-})
-
-app.use((error, req, res, next) => {
-    if (res.headerSent) {
-        return next(error)
-    }
-    res.status(error.code || 500);
-    res.json({ message: error.message || 'An unknown error occured' });
+  throw new HttpError('Could not find the route', 404);
 });
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occured' });
+});
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('Connected');
+app.listen(5000, () => {
+  console.log('Connected');
 });
